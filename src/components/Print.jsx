4e456cf0668d58f3f11f3402c4437e4ced19c535
AadeshLawange-Guardinger <aadesh.lawange@guardinger.in>
2024-05-31 16:34:39 +0530

    Report updated.

--- src/components/Print.jsx
+++ src/components/Print.jsx
@@ -51,9 +51,68 @@ export default function Print() {
 
   // Define custom blinking red dot icon
   const blinkingRedDotIcon = L.divIcon({
-    className: "blinking-dot-icon",
-    iconSize: [12, 12],
+    className: "map-dot-icon",
+    iconSize: [50, 50],
   });
+
+  // Remove the degree symbol from doa
+  let doaPlot = doa.replace("°", "");
+
+  // Prepare data for the polar chart
+  const data = [
+    {
+      type: "scatterpolar",
+      r: [0.8], // Magnitude of the direction
+      theta: [doaPlot], // Direction of arrival
+      mode: "markers",
+      marker: {
+        size: 10,
+        color: "#018C8C", // Customize the color of the marker
+      },
+    },
+    {
+      type: "scatterpolar",
+      r: [0, 0.75], // Line from origin to the point
+      theta: [0, doaPlot],
+      mode: "lines",
+      line: {
+        color: "red", // Color of the line
+        dash: "dot", // Make the line dotted
+      },
+    },
+  ];
+
+  // Layout configuration for the polar chart
+  const layout = {
+    polar: {
+      radialaxis: {
+        visible: true,
+        range: [0, 1], // Set the range of radial axis
+        tickvals: [0, 90, 180, 270], // Define the tick values
+      },
+    },
+    // Hide legends
+    showlegend: false,
+    // Hide the coordinates
+    hoverinfo: "none",
+    // Configure modebar to be hidden
+    config: {
+      displayModeBar: false,
+    },
+    width: 1250,
+    height: 508,
+    margin: {
+      t: 50,
+      b: 50,
+      l: 50,
+      r: 50,
+    },
+    font: {
+      color: "#000000",
+    },
+    plot_bgcolor: "transparent",
+    paper_bgcolor: "transparent",
+  };
 
   // Function to handle when all plots are rendered
   const handlePlotsLoaded = () => {
@@ -61,7 +120,7 @@ export default function Print() {
   };
 
   const handlePrintRequest = () => {
-    const elements = document.querySelectorAll(".page-1,.page-2");
+    const elements = document.querySelectorAll(".page-1, .page-2, .page-3");
     const timestamp = new Date()
       .toLocaleString("en-US", {
         year: "numeric",
@@ -77,87 +136,85 @@ export default function Print() {
       .toLowerCase();
 
     const pdf = new jsPDF({
-      orientation: "portrait", // Set orientation to portrait
+      orientation: "portrait",
     });
 
-    let posY = 20; // Set initial Y position with space from top
+    const addHeader = (pdf, img) => {
+      pdf.setFillColor(255, 255, 255); // White background
+      pdf.rect(0, 0, 210, 30, "F"); // Rectangle for header background
+      pdf.addImage(img, "JPEG", 10, 1, 32, 35); // Logo image
 
-    // Add header to the first page
-    // Add header to the first page
-    const addHeader = (pageNumber) => {
-      // Set header background color
-      pdf.setFillColor(255, 255, 255); // #004367 in RGB
+      const dateString = new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+      const timeString = new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+        timeZone: "Asia/Kolkata",
+      });
 
-      // Fill a rectangle as the header background
-      pdf.rect(0, 0, 210, 30, "F"); // (x, y, width, height, style)
-
-      const img = new Image();
-      img.src = wesee_logo; // Assuming wesee_logo is the imported image
-      img.onload = function () {
-        pdf.addImage(this, "JPEG", 10, 1, 32, 25); // Add the image to the header
-
-        // Add timestamp to the right corner
-        const dateOptions = {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        };
-        const timeOptions = {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false,
-          timeZone: "Asia/Kolkata",
-        };
-        const dateString = new Date().toLocaleDateString("en-US", dateOptions);
-        const timeString = new Date().toLocaleTimeString("en-US", timeOptions);
-
-        pdf.setFontSize(10);
-        pdf.setTextColor(0);
-        pdf.text(
-          `${dateString} ${timeString} (IST)`,
-          205,
-          10,
-          null,
-          null,
-          "right"
-        ); // Add timestamp to the right corner
-      };
-      img.onerror = function () {
-        console.error("Error loading image.");
-      };
+      pdf.setFontSize(10);
+      pdf.setTextColor(0);
+      pdf.text(`${dateString} ${timeString} (IST)`, 205, 10, {
+        align: "right",
+      });
     };
 
-    Promise.all(
-      Array.from(elements).map((element, index) =>
-        html2canvas(element, {
-          useCORS: true,
-        }).then((canvas) => {
-          const imgData = canvas.toDataURL("image/png");
-          const width = 210; // Width of A4 in mm
-          const height = (canvas.height * width) / canvas.width; // Maintain aspect ratio
-          if (index !== 0) {
-            pdf.addPage(); // Add a new page for subsequent elements
-            posY = 20; // Reset posY for new page with space from top
-          } else {
-            addHeader(1); // Add header to the first page
-          }
-          pdf.addImage(imgData, "PNG", 0, posY + 20, width, height); // Add image to PDF with adjusted Y position
-          posY += height + 40; // Update Y position for next element with space between elements
-        })
-      )
-    ).then(() => {
-      const storedData = localStorage.getItem("reportData");
-      pdf.save(`report_${bouyId}_${timestamp}.pdf`); // Download PDF
-      if (storedData) {
-        // Parse the stored data back to an object
-        const data = JSON.parse(storedData);
+    const loadImage = (src) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(err);
+      });
+    };
 
-        // Print data to console
-        //console.log("Data from localStorage:", data);
-        navigate("/viewreport", { state: data }); // Pass data as state
-      }
-    });
+    loadImage(wesee_logo)
+      .then((img) => {
+        const promises = Array.from(elements).map((element, index) =>
+          html2canvas(element, { useCORS: true }).then((canvas) => {
+            const imgData = canvas.toDataURL("image/png");
+            const width = 210; // Width of A4 in mm
+            const height = (canvas.height * width) / canvas.width; // Maintain aspect ratio
+
+            if (index !== 0) {
+              pdf.addPage(); // New page for subsequent elements
+            }
+
+            if (element.classList.contains("page-1") && index === 0) {
+              addHeader(pdf, img); // Add header only to the first page
+            }
+
+            let { posY } = 10;
+
+            if (element.classList.contains("page-1") && index === 0) {
+              posY = 20; // Initial Y position with space from top
+            } else {
+              posY = 0;
+            }
+
+            pdf.addImage(imgData, "PNG", 0, posY + 20, width, height); // Add image to PDF
+          })
+        );
+
+        return Promise.all(promises);
+      })
+      .then(() => {
+        const storedData = localStorage.getItem("reportData");
+        pdf.save(`report_${bouyId}_${timestamp}.pdf`); // Download PDF
+
+        if (storedData) {
+          const data = JSON.parse(storedData);
+          navigate("/viewreport", { state: data }); // Pass data as state
+        }
+      })
+      .catch((error) => {
+        console.error("Error generating PDF:", error);
+      });
   };
 
   return (
@@ -220,34 +277,6 @@ export default function Print() {
               </table>
             </div>
           )}
-          <div className="print-plot-container">
-            <h1>Waveform (Time vs Amplitude)</h1>
-            {waveformData && (
-              <Plot
-                data={[
-                  {
-                    x: waveformData.t_waveform,
-                    y: waveformData.audio_reconstructed_waveform,
-                    type: "scatter",
-                    mode: "lines",
-                  },
-                ]}
-                layout={{
-                  width: 1200,
-                  height: 500,
-                  xaxis: {
-                    title: "Time (sec)",
-                  },
-                  yaxis: {
-                    title: "Amplitude (Volts)",
-                  },
-                }}
-                config={{ displayModeBar: false }}
-                onInitialized={handlePlotsLoaded} // Call when plot is rendered
-                onUpdate={handlePlotsLoaded} // Call when plot is updated
-              />
-            )}
-          </div>
         </div>
       </div>
       <div className="page-2">
@@ -256,6 +285,7 @@ export default function Print() {
             <h1>Short Time Fourier Transform (STFT)</h1>
             {lofarData && (
               <Plot
+                className="plots-container"
                 data={[
                   {
                     x: lofarData.F.map((row) => row[0]),
@@ -300,6 +330,7 @@ export default function Print() {
             <h1>Power Spectral Density (PSD)</h1>
             {psdData && (
               <Plot
+                className="plots-container"
                 data={[
                   {
                     x: psdData.freqs_psd,
@@ -330,39 +361,72 @@ export default function Print() {
               />
             )}
           </div>
-          {/* <div className="print-plot-container">
-          <h1>Sonabuoy Location</h1>
-          {mapData && (
-            <MapContainer
-              className="world-map-container dark"
-              center={[mapData.lat, mapData.long]}
-              zoom={13} // You can adjust the default zoom level here
-              style={{ height: "500px", width: "100%" }}
-            >
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Circle
-                center={[mapData.lat, mapData.long]}
-                radius={1000}
-                fillOpacity={0.4}
-                weight={0}
-                opacity={0.4}
+          <div className="print-plot-container">
+            <h1>Direction Of Arrival (DOA)</h1>
+            {psdData && (
+              <Plot
+                className="plots-container"
+                data={data}
+                layout={layout}
+                config={{ displayModeBar: false }}
               />
-              <Marker
-                position={[mapData.lat, mapData.long]}
-                icon={blinkingRedDotIcon}
-              >
-                <Popup>Direction of Arrival: {mapData.doa}</Popup>
-              </Marker>
-            </MapContainer>
-          )}
-        </div> */}
-
-          <div className="remark-print">Remarks:</div>
+            )}
+          </div>
         </div>
-        {/* <div className="authorization-notice">
-          This report is authorized by The R&D establishment of the Indian Navy
-          – “WESEE”.
-        </div> */}
+      </div>
+      <div className="page-3">
+        <div className="print-container">
+          <div className="print-plot-container">
+            <h1>Waveform (Time vs Amplitude)</h1>
+            {waveformData && (
+              <Plot
+                className="plots-container"
+                data={[
+                  {
+                    x: waveformData.t_waveform,
+                    y: waveformData.audio_reconstructed_waveform,
+                    type: "scatter",
+                    mode: "lines",
+                  },
+                ]}
+                layout={{
+                  width: 1200,
+                  height: 500,
+                  xaxis: {
+                    title: "Time (sec)",
+                  },
+                  yaxis: {
+                    title: "Amplitude (Volts)",
+                  },
+                }}
+                config={{ displayModeBar: false }}
+                onInitialized={handlePlotsLoaded} // Call when plot is rendered
+                onUpdate={handlePlotsLoaded} // Call when plot is updated
+              />
+            )}
+          </div>
+          <div className="print-plot-container">
+            <h1>Sonabuoy Location</h1>
+            {mapData && (
+              <MapContainer
+                className="world-map-container dark"
+                center={[mapData.lat, mapData.long]}
+                zoom={14.5} // You can adjust the default zoom level here
+                style={{ height: "500px", width: "100%" }}
+              >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker
+                  position={[mapData.lat, mapData.long]}
+                  icon={blinkingRedDotIcon}
+                >
+                  <Popup>Direction of Arrival: {mapData.doa}</Popup>
+                </Marker>
+              </MapContainer>
+            )}
+          </div>
+          <div className="remark-print">Remarks:</div>
+          <div className="authorization-notice">Authorized Signatory</div>
+        </div>
       </div>
       {isLoading ? (
         <div>Loading...</div>
